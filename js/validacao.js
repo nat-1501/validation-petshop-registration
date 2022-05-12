4f40e6a5e1cff56814f1be0c4605983a3307cf2c
Nat-1501 <natalisoares90@gmail.com>
2022-05-12 13:15:01 -0300

    Validando Cpf com numeros repetidos

--- js/validacao.js
+++ js/validacao.js
@@ -38,13 +38,18 @@ const mensagensDeErro = {
     dataNascimento: {
         valueMissing: 'O campo da data de nascimento não pode estar vazio.',
         customError: 'Você deve ser maior que 18 anos para se cadastrar.'
+    },
+    cpf: {
+        valueMissing: 'O campo de CPF não pode estar vazio.',
+        customError: 'O CPF digitado não é valido.'
     }
     
 }
 
 
 const validadores = {
-    dataNascimento:input => validaDataNascimento(input)
+    dataNascimento:input =>  validaDataNascimento(input),
+    cpf:input => validaCPF(input)
 }
 
 function mostraMensagemDeErro(tipoDeInput, input) {
@@ -76,3 +81,40 @@ function maiorQue18(data) {
     return dataMais18 <= dataAtual
 
 }
+
+function validaCPF(input) {
+    const cpfFormatado = input.value.replace(/\D/g, '')
+    let mensagem = ''
+
+    if(!checaCPFRepetido(cpfFormatado)) {
+        mensagem = 'O CPF digitado não é valido.'
+    }
+
+    input.setCustomValidity(mensagem)
+}
+
+function checaCPFRepetido(cpf) {
+    const valoresRepetidos = [
+        '00000000000',
+        '11111111111',
+        '22222222222' ,  
+        '33333333333',
+        '44444444444',
+        '55555555555',
+        '66666666666',
+        '77777777777',
+        '88888888888',
+        '99999999999'
+    ]
+
+    let cpfValido = true
+
+    valoresRepetidos.forEach(valor => {
+        if(valor == cpf) {
+            cpfValido = false
+        }
+    })
+
+    return cpfValido
+
+}    
